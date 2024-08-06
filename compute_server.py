00d6a3cffbce07064232a475355e08c84d79bc4a
Adarsh321123 <adarshk1234567@gmail.com>
2024-08-05 19:11:31 -0700

    condense requirements + change print to logger.info in compute_server.py to be seen on runpod

--- compute_server.py
+++ compute_server.py
@@ -21,6 +21,7 @@ import numpy as np
 import sys
 import traceback
 import time
+from loguru import logger
 
 ROOT_DIR = os.environ.get('ROOT_DIR', '/workspace')
 DATA_DIR = os.environ.get('DATA_DIR', 'datasets')
@@ -38,19 +39,19 @@ def merge_datasets():
     split_types = ['train', 'val', 'test']
 
     for strategy in split_strategies:
-        print(f"Merging datasets for {strategy}")
+        logger.info(f"Merging datasets for {strategy}")
         strategy_dir = merged_dir + "/" + strategy
         if not os.path.exists(strategy_dir):
             os.makedirs(strategy_dir)
 
         for split in split_types:
-            print(f"Processing {split} split")
+            logger.info(f"Processing {split} split")
             merged_data = {}
             
             for dataset in os.listdir(data_dir):
                 if dataset == "merged":
                     continue
-                print(f"Processing {dataset}")
+                logger.info(f"Processing {dataset}")
                 dataset_path = os.path.join(data_dir, dataset)
                 if os.path.isdir(dataset_path):
                     json_file = os.path.join(dataset_path, strategy, f"{split}.json")
@@ -66,15 +67,15 @@ def merge_datasets():
             with open(output_file, 'w') as f:
                 json.dump(list(merged_data.values()), f)
             
-            print(f"Finished processing {split} split")
-        print(f"Finished merging datasets for {strategy}")
+            logger.info(f"Finished processing {split} split")
+        logger.info(f"Finished merging datasets for {strategy}")
     
-    print("Merging corpus")
+    logger.info("Merging corpus")
     merged_corpus = {}
     for dataset in os.listdir(data_dir):
         if dataset == "merged":
             continue
-        print(f"Processing {dataset}")
+        logger.info(f"Processing {dataset}")
         dataset_path = os.path.join(data_dir, dataset)
         if os.path.isdir(dataset_path):
             corpus_file = os.path.join(dataset_path, "corpus.jsonl")
@@ -90,14 +91,14 @@ def merge_datasets():
         for line in merged_corpus.values():
             f.write(line + "\n")
 
-    print("Finished merging corpus")
+    logger.info("Finished merging corpus")
 
-    print("Adding metadata")
+    logger.info("Adding metadata")
 
     for dataset in os.listdir(data_dir):
         if dataset == "merged":
             continue
-        print(f"Getting metadata from {dataset}")
+        logger.info(f"Getting metadata from {dataset}")
         dataset_path = os.path.join(data_dir, dataset)
         if os.path.isdir(dataset_path):
             metadata_file = os.path.join(dataset_path, "metadata.json")
@@ -106,13 +107,13 @@ def merge_datasets():
                     json.dump(json.load(open(metadata_file)), f)
                     break
     
-    print("Finished adding metadata")
+    logger.info("Finished adding metadata")
 
-    print("Deleting individual datasets")
+    logger.info("Deleting individual datasets")
     for dataset in os.listdir(data_dir):
         dataset_path = os.path.join(data_dir, dataset)
         if os.path.isdir(dataset_path) and dataset != "merged":
-            print(f"Deleting dataset: {dataset}")
+            logger.info(f"Deleting dataset: {dataset}")
             shutil.rmtree(dataset_path)
 
 def get_compatible_commit(url):
@@ -128,11 +129,11 @@ def get_compatible_commit(url):
         config = repo.get_config("lean-toolchain")
         v = generate_benchmark_lean4.get_lean4_version_from_config(config["content"])
         if generate_benchmark_lean4.is_supported_version(v):
-            print(f"Latest commit compatible for url {url}")
+            logger.info(f"Latest commit compatible for url {url}")
             return latest_commit, v
 
         # Search for latest commit on v4.9.1
-        print(f"Searching for compatible commit for {url}")
+        logger.info(f"Searching for compatible commit for {url}")
         process = subprocess.Popen(
             ["git", "fetch", "--depth=1000000", url],  # Fetch commits
             stdout=subprocess.PIPE,
@@ -161,34 +162,34 @@ def get_compatible_commit(url):
         raise Exception("No compatible commit found")
 
     except Exception as e:
-        print(f"Error in get_compatible_commit: {str(e)}")
+        logger.info(f"Error in get_compatible_commit: {str(e)}")
         return None, None
 
 def generate_dataset(unique_urls):
     # TODO: optimize to not make novel_premises split
     # TODO: no need for licenses or metadata
     # TODO: do we need this leangitrepo stuff?
-    print(f"Generating {len(unique_urls)} datasets")
+    logger.info(f"Generating {len(unique_urls)} datasets")
     for url in unique_urls:
         if not url.endswith('.git'):
             url = url + '.git'
 
         sha, v = get_compatible_commit(url)
         if not sha:
-            print(f"Failed to find a compatible commit for {url}")
+            logger.info(f"Failed to find a compatible commit for {url}")
             continue
-        print(f"Found compatible commit {sha} for {url}")
-        print(f"Lean version: {v}")
+        logger.info(f"Found compatible commit {sha} for {url}")
+        logger.info(f"Lean version: {v}")
 
         url = url.replace('.git', '')
         repo = LeanGitRepo(url, sha)
         dir_name = repo.url.split("/")[-1] + "_" + sha
         dst_dir = ROOT_DIR + "/" + DATA_DIR + "/" + dir_name
         generate_benchmark_lean4.main(repo.url, sha, dst_dir)
-        print(f"Finished generating benchmark at {dst_dir}")
-    print("Merging datasets")
+        logger.info(f"Finished generating benchmark at {dst_dir}")
+    logger.info("Merging datasets")
     merge_datasets()
-    print("Finished merging datasets")
+    logger.info("Finished merging datasets")
 
 
 def get_recent_pl_checkpoint():
@@ -196,7 +197,7 @@ def get_recent_pl_checkpoint():
     models = response.json()
 
     if not models:
-        print("No checkpoints found")
+        logger.info("No checkpoints found")
         return None
 
     sorted_models = sorted(models, key=lambda x: x['createdAt'], reverse=True)
@@ -204,7 +205,7 @@ def get_recent_pl_checkpoint():
         if "pl" in model['modelId']:
             return model['modelId']
     
-    print("No PL checkpoints found")
+    logger.info("No PL checkpoints found")
     return None
 
 def download_pl_checkpoint(model_id):
@@ -213,10 +214,10 @@ def download_pl_checkpoint(model_id):
         os.makedirs(save_directory)
     try:
         cache_path = hf_hub_download(repo_id=model_id, filename="model.ckpt")
-        print(f"Checkpoint downloaded to: {cache_path}")
+        logger.info(f"Checkpoint downloaded to: {cache_path}")
         return cache_path
     except Exception as e:
-        print(f"Error downloading or saving checkpoint: {e}")
+        logger.info(f"Error downloading or saving checkpoint: {e}")
         return None
 
 def upload_best_models_to_hf(pl_path, ct2_path, embeddings_path, next_suffix):
@@ -233,7 +234,7 @@ def upload_best_models_to_hf(pl_path, ct2_path, embeddings_path, next_suffix):
             token=api.token
         )
         
-        print(f"Uploaded PyTorch Lightning checkpoint to {pl_repo_id}")
+        logger.info(f"Uploaded PyTorch Lightning checkpoint to {pl_repo_id}")
 
         ct2_model_name = f"ct2-leancopilot-{next_suffix}"
         ct2_repo_id = f"{USER}/{ct2_model_name}"
@@ -244,7 +245,7 @@ def upload_best_models_to_hf(pl_path, ct2_path, embeddings_path, next_suffix):
             token=api.token
         )
 
-        print(f"Uploaded CTranslate2 checkpoint to {ct2_repo_id}")
+        logger.info(f"Uploaded CTranslate2 checkpoint to {ct2_repo_id}")
 
         emb_model_name = f"emb-leancopilot-{next_suffix}"
         emb_repo_id = f"{USER}/{emb_model_name}"
@@ -254,12 +255,12 @@ def upload_best_models_to_hf(pl_path, ct2_path, embeddings_path, next_suffix):
             repo_id=emb_repo_id,
             token=api.token
         )
-        print(f"Uploaded embeddings to {emb_repo_id}")
+        logger.info(f"Uploaded embeddings to {emb_repo_id}")
 
-        print(f"Successfully uploaded all files to separate repositories")
+        logger.info(f"Successfully uploaded all files to separate repositories")
         return pl_repo_id, ct2_repo_id, emb_repo_id
     except Exception as e:
-        print(f"Error uploading checkpoint to Hugging Face: {e}")
+        logger.info(f"Error uploading checkpoint to Hugging Face: {e}")
         return None
     
 def convert_to_hf(best_model_path, hf_path) -> None:
@@ -284,7 +285,7 @@ def get_premise_embeddings(indexed_corpus_path, embeddings_path):
 
     npy_path = embeddings_path + "/" + "embeddings.npy"
     np.save(npy_path, embeddings_array_64)
-    print(f"Embeddings saved to {npy_path}")
+    logger.info(f"Embeddings saved to {npy_path}")
 
     all_premises = indexed_corpus.corpus.all_premises
 
@@ -295,11 +296,11 @@ def get_premise_embeddings(indexed_corpus_path, embeddings_path):
 
     dict_path = embeddings_path + "/" + "dictionary.json"
     json.dump(premise_dict, open(dict_path, "wt"), indent=4)
-    print(f"Dictionary saved to {dict_path}")
+    logger.info(f"Dictionary saved to {dict_path}")
 
 def index_corpus(model_checkpoint_path, corpus_path, batch_size, embeddings_path):
     if not torch.cuda.is_available():
-        print("Indexing the corpus using CPU can be very slow.")
+        logger.info("Indexing the corpus using CPU can be very slow.")
         device = torch.device("cpu")
     else:
         device = torch.device("cuda")
@@ -311,55 +312,55 @@ def index_corpus(model_checkpoint_path, corpus_path, batch_size, embeddings_path
         IndexedCorpus(model.corpus, model.corpus_embeddings.to(torch.float32).cpu()),
         open(embeddings_path, "wb"),
     )
-    print(f"Indexed corpus saved to {embeddings_path}")
+    logger.info(f"Indexed corpus saved to {embeddings_path}")
 
 def convert_and_upload_models(best_model_path, new_data_path, next_suffix):
     if best_model_path:
-        print(f"Best model path: {best_model_path}")
+        logger.info(f"Best model path: {best_model_path}")
 
-        print("Converting best model to HuggingFace format")
+        logger.info("Converting best model to HuggingFace format")
         hf_path = ROOT_DIR + "/" + CHECKPOINT_DIR + "/" + "hf-leancopilot-" + str(next_suffix)
         convert_to_hf(best_model_path, hf_path)
-        print(f"Converted model to HuggingFace format at: {hf_path}")
+        logger.info(f"Converted model to HuggingFace format at: {hf_path}")
 
-        print("Converting HuggingFace model to CTranslate2 format")
+        logger.info("Converting HuggingFace model to CTranslate2 format")
         ct2_path = ROOT_DIR + "/" + CHECKPOINT_DIR + "/" + "ct2-leancopilot-" + str(next_suffix)
         convert_t5encoder_to_ct2.main(hf_path, ct2_path)
-        print(f"Converted HuggingFace model to CTranslate2 format at: {ct2_path}")
+        logger.info(f"Converted HuggingFace model to CTranslate2 format at: {ct2_path}")
 
         # TODO: no need to pickle and then unpickle right after
-        print("Indexing the corpus to get premise embeddings")
+        logger.info("Indexing the corpus to get premise embeddings")
         corpus_path = new_data_path + "/corpus.jsonl"
         batch_size = 64
         indexed_corpus_path = ROOT_DIR + "/" + CHECKPOINT_DIR + "/" + "indexed_corpus.pkl"
         index_corpus(best_model_path, corpus_path, batch_size, indexed_corpus_path)
-        print("Finished indexing the corpus")
+        logger.info("Finished indexing the corpus")
 
-        print("Getting premise embeddings")
+        logger.info("Getting premise embeddings")
         embeddings_path = ROOT_DIR + "/" + CHECKPOINT_DIR + "/" + "emb-leancopilot-" + str(next_suffix)
         if not os.path.exists(embeddings_path):
             os.makedirs(embeddings_path)
         get_premise_embeddings(indexed_corpus_path, embeddings_path)
-        print(f"Premise embeddings saved to: {embeddings_path}")
+        logger.info(f"Premise embeddings saved to: {embeddings_path}")
 
-        print("Uploading best models to Hugging Face as PL, CT2, and embeddings")
+        logger.info("Uploading best models to Hugging Face as PL, CT2, and embeddings")
         pl_repo_id, ct2_repo_id, emb_repo_id = upload_best_models_to_hf(best_model_path, ct2_path, embeddings_path, next_suffix)
         if pl_repo_id and ct2_repo_id and emb_repo_id:
-            print(f"Uploaded PyTorch Lightning model to: {pl_repo_id}")
-            print(f"Uploaded CTranslate2 model to: {ct2_repo_id}")
-            print(f"Uploaded embeddings to: {emb_repo_id}")
+            logger.info(f"Uploaded PyTorch Lightning model to: {pl_repo_id}")
+            logger.info(f"Uploaded CTranslate2 model to: {ct2_repo_id}")
+            logger.info(f"Uploaded embeddings to: {emb_repo_id}")
         else:
-            print("Failed to upload one or more models to Hugging Face")
+            logger.info("Failed to upload one or more models to Hugging Face")
     else:
-        print("No best model found")
+        logger.info("No best model found")
     
 # TODO: reduce duplication with main.py
 def train(model_checkpoint_path, new_data_path, next_suffix, max_epochs=1): # TODO: chagne back to 2
-    print(f"Training model with checkpoint: {model_checkpoint_path}")
+    logger.info(f"Training model with checkpoint: {model_checkpoint_path}")
     seed_everything(3407)
 
     if not torch.cuda.is_available():
-        print("Indexing the corpus using CPU can be very slow.")
+        logger.info("Indexing the corpus using CPU can be very slow.")
         device = torch.device("cpu")
     else:
         device = torch.device("cuda")
@@ -376,11 +377,11 @@ def train(model_checkpoint_path, new_data_path, next_suffix, max_epochs=1): # TO
     model = PremiseRetriever.load(
         model_checkpoint_path, device, freeze=False, config=config
     )
-    print(f"Loaded premise retriever at {model_checkpoint_path}")
+    logger.info(f"Loaded premise retriever at {model_checkpoint_path}")
 
     corpus_path = new_data_path + "/corpus.jsonl"
     data_path = new_data_path + "/random"
-    print(f"Data path: {data_path}")
+    logger.info(f"Data path: {data_path}")
     data_module = RetrievalDataModule(
         data_path=data_path,
         corpus_path=corpus_path,
@@ -414,9 +415,9 @@ def train(model_checkpoint_path, new_data_path, next_suffix, max_epochs=1): # TO
 
     lr_monitor = LearningRateMonitor(logging_interval='step')
 
-    print(f"Training dataset size after load: {len(data_module.ds_train)}")
-    print(f"Validation dataset size after load: {len(data_module.ds_val)}")
-    print(f"Testing dataset size after load: {len(data_module.ds_pred)}")
+    logger.info(f"Training dataset size after load: {len(data_module.ds_train)}")
+    logger.info(f"Validation dataset size after load: {len(data_module.ds_val)}")
+    logger.info(f"Testing dataset size after load: {len(data_module.ds_pred)}")
 
     trainer = pl.Trainer(
         accelerator="gpu",
@@ -430,14 +431,14 @@ def train(model_checkpoint_path, new_data_path, next_suffix, max_epochs=1): # TO
         num_sanity_val_steps=0, # TODO: remove later
     )
 
-    print("Starting progressive training...")
+    logger.info("Starting progressive training...")
 
     trainer.fit(model, datamodule=data_module, ckpt_path=model_checkpoint_path)
 
-    print("Finished training")
+    logger.info("Finished training")
 
     best_model_path = checkpoint_callback.best_model_path
-    print("Converting and uploading models")
+    logger.info("Converting and uploading models")
     convert_and_upload_models(best_model_path, new_data_path, next_suffix)
 
 def fetch_urls_from_api(api_url):
@@ -446,7 +447,7 @@ def fetch_urls_from_api(api_url):
         response.raise_for_status()
         return response.json()["urls"]
     except requests.RequestException as e:
-        print(f"Error fetching URLs from API: {e}")
+        logger.info(f"Error fetching URLs from API: {e}")
         return []
 
 def main():
@@ -455,22 +456,22 @@ def main():
     # TODO: put a try-catch around generate benchmark, and anything else that could fail, same with main.py
     # TODO: should we just not cache the datasets so we can save space?
     try:
-        print("Starting compute server...")
+        logger.info("Starting compute server...")
         if ray.is_initialized():
             ray.shutdown()
         
         if not os.path.exists(ROOT_DIR + "/" + DATA_DIR):
             os.makedirs(ROOT_DIR + "/" + DATA_DIR)
 
-        print("GitHub Token:", os.environ.get('GITHUB_ACCESS_TOKEN')) # TODO: remove
+        logger.info("GitHub Token:", os.environ.get('GITHUB_ACCESS_TOKEN')) # TODO: remove
         api_url = "https://leancopilotapi.onrender.com"
         unique_urls = set(fetch_urls_from_api(api_url))
-        print(f"Unique URLs: {unique_urls}")
-        print("About to generate datasets...")
+        logger.info(f"Unique URLs: {unique_urls}")
+        logger.info("About to generate datasets...")
 
         # TODO: container keeps restarting, need to fix
         # while True: # TODO: remove
-        #     print("Running main process...")
+        #     logger.info("Running main process...")
         #     time.sleep(10)
 
         # return # TODO: remove
@@ -479,19 +480,19 @@ def main():
 
         model_id = get_recent_pl_checkpoint()
         if model_id:
-            print(f"Latest PL checkpoint found: {model_id}")
+            logger.info(f"Latest PL checkpoint found: {model_id}")
             model_checkpoint_path = download_pl_checkpoint(model_id)
             if model_checkpoint_path:
-                print(f"Checkpoint path: {model_checkpoint_path}")
+                logger.info(f"Checkpoint path: {model_checkpoint_path}")
                 merged_data_path = ROOT_DIR + "/" + DATA_DIR + "/" + "merged"
                 next_suffix = int(model_id.split("-")[-1]) + 1
                 train(model_checkpoint_path, merged_data_path, next_suffix)
             else:
-                print("Error downloading or saving checkpoint")
+                logger.info("Error downloading or saving checkpoint")
         else:
-            print("No checkpoints found")
+            logger.info("No checkpoints found")
     except Exception as e:
-        print(f"An error occurred: {e}", file=sys.stderr)
+        logger.info(f"An error occurred: {e}", file=sys.stderr)
         traceback.print_exc()
 
 if __name__ == "__main__":
