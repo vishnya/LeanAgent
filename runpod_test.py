597070cf6f8cdc457bbdae0e5fc9f60702ccde1a
Adarsh321123 <adarshk1234567@gmail.com>
2024-08-05 18:34:54 -0700

    small script to test running on runpod

---
+++ runpod_test.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    print("Hello, World!")
+    print("This is a test script.")
+    print("This script is used to test the runpod module.")
+    print("The runpod module is used to run a pod")
