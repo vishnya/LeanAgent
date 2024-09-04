acf5c7d80d2ac1d2b9679ae73d1061a6570fbc96
gray311 <g19myz@gmail.com>
2024-09-04 21:51:03 +0000

    overwrite config

--- retrieval/main.py
+++ retrieval/main.py
@@ -8,13 +8,18 @@ import pickle
 import json
 from tqdm import tqdm
 from loguru import logger
-from pytorch_lightning.cli import LightningCLI
+from pytorch_lightning.cli import LightningCLI, SaveConfigCallback
 import sys
 
 from retrieval.model import PremiseRetriever
 from retrieval.datamodule import RetrievalDataModule
 
 class CLI(LightningCLI):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, 
+                         save_config_kwargs={"overwrite": True},
+                         **kwargs)
+
     def add_arguments_to_parser(self, parser) -> None:
         parser.link_arguments("model.model_name", "data.model_name")
         parser.link_arguments("data.max_seq_len", "model.max_seq_len")
