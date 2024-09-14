6f02e4a8a38a84735bef72c9ab676c5ebf499c76
gray311 <g19myz@gmail.com>
2024-09-14 00:42:50 +0000

    add sh for runnign fisher comp

---
+++ run_code_compute_fisher.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export RAID_DIR="/data/yingzi_ma/lean_project"
+cd ${RAID_DIR}/ReProver
+echo "Script executed from: ${PWD}"
+source ${RAID_DIR}/../miniconda3/etc/profile.d/conda.sh
+conda activate ReProver
+export PYTHONPATH="${PYTHONPATH}:${RAID_DIR}/ReProver"
+export GITHUB_ACCESS_TOKEN="<>"
+export CACHE_DIR="${RAID_DIR}/.cache/lean_dojo"
+echo "Removing old cache files"
+rm -rf /tmp/ray
+echo "Stopping ray"
+ray stop --force
+echo "Running compute_fisher.py"
+python compute_fisher.py
